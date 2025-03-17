887e4070af4c22cf74f6fd60a4e26d7009d7df44
Bailin He <bahe@equinix.com>
2025-03-17 13:56:52 +0000

    use BSL version
    Signed-off-by: Bailin He <bahe@equinix.com>

--- internal/dbtools/testing.go
+++ internal/dbtools/testing.go
@@ -3,7 +3,8 @@ package dbtools
 import "github.com/cockroachdb/cockroach-go/v2/testserver"
 
 // TestServerCRDBVersion is the version of CockroachDB that the test server is running
-const TestServerCRDBVersion = "v24.3.8"
+// v23.1.28 is the last version under BSL
+const TestServerCRDBVersion = "v23.1.28"
 
 // NewCRDBTestServer creates a new CockroachDB test server
 func NewCRDBTestServer() (testserver.TestServer, error) {
